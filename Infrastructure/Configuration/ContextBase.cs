9378ce3ae1a9a57156cbf7f2f5319510dfd9a487
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-10 19:16:45 -0300

    create infrastructure context and repository

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase( DbContextOptions<ContextBase> options) : base(options)
+        {                
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Data Source = HEITOR\\SQLEXPRESS; Initial Catalog = EcoIBYPOWER; Integrated Security = True";
+            return strCon;
+        }
+    }
+}
---
+++ Infrastructure/Repository/Repositories/RepositoryProduct.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Entities.Entities;
+using Infrastructure.Repository.Generics;
+
+namespace Infrastructure.Repository.Repositories
+{
+    public class RepositoryProduct : RepositoryGenerics<Product>, IProduct
+    {
+    }
+}
